c29bbe87e33daec28a4ef9d13d4c7457765de56f
Ankush Dongre <121801285+AnkushDongare@users.noreply.github.com>
2023-08-03 14:02:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,31 @@
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv"); // Require the dotenv package
-const fs = require("fs");
+const dotenv = require("dotenv");
+const fs = require("fs").promises; // Use the 'promises' version of fs
 const app = express();
-dotenv.config(); // Load environment variables from .env
+dotenv.config();
 
 const port = process.env.PORT || 6000;
-// Middleware: CORS
 app.use(cors());
-
 app.use(express.json());
 
-// Routes
-app.get("/data1", (req, res) => {
-  readAndSendData("data1.json", res);
-});
+// Define common parts of the URL as variables
+const shootingResultsRoute = "/shooting-results";
+const openSightRoute = "/open-sight";
+const pipSightRoute = "/pip-sight";
+const pistolRoute = "/pistol";
+const urbanRoute = "/urban";
+const ruralRoute = "/rural";
+const maleRoute = "/male";
+const femaleRoute = "/female";
+const u14Route = "/u14";
+const u17Route = "/u17";
+const u19Route = "/u19";
 
-app.get("/data2", (req, res) => {
-  readAndSendData("data2.json", res);
-});
-
-app.get("/data3", (req, res) => {
-  readAndSendData("data3.json", res);
-});
-
-// Add more routes for other JSON files as needed.
-
-function readAndSendData(filename, res) {
+// Define a helper function to read and send data
+async function readAndSendData(filename, res) {
   try {
-    // Read data from the JSON file synchronously
-    const fileData = fs.readFileSync(filename, "utf8");
+    const fileData = await fs.readFile(filename, "utf8");
     const data = JSON.parse(fileData);
     console.log(`Data loaded successfully from ${filename}`);
     res.send(data);
@@ -39,6 +35,30 @@ function readAndSendData(filename, res) {
   }
 }
 
+// Define routes using async/await and variables
+app.get(`/data`, async (req, res) => {
+  await readAndSendData("data1.json", res);
+});
+
+// app.get(`${shootingResultsRoute}${openSightRoute}${urbanRoute}${maleRoute}${u14Route}`, async (req, res) => {
+//   await readAndSendData("Urban-OpenSight-Male-U14.json", res);
+// });
+
+// app.get(`${shootingResultsRoute}${openSightRoute}${urbanRoute}${maleRoute}${u17Route}`, async (req, res) => {
+//   await readAndSendData("Urban-OpenSight-Male-U17.json", res);
+// });
+
+// app.get(`${shootingResultsRoute}${openSightRoute}${urbanRoute}${maleRoute}${u19Route}`, async (req, res) => {
+//   await readAndSendData("Urban-OpenSight-Male-U19.json", res);
+// });
+
+// Add more routes using the same pattern as needed.
+
+app.get("*", (req, res) => {
+  res.status(404).send("Invalid route");
+});
+
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
